d93ac773a42cb793d62d76c4070f1b217562bc8b
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-09-11 18:29:16 +0200

    add new project and gh-pages settings

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: { host: "0.0.0.0" },
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+    },
+  },
+  base: "/WordsCraft/",
+  envPrefix: "APP_",
+});
